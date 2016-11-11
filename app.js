61d1a6020c45ec9ff5b04c886f5d251d36af6705
salus-sage <salus.sage@gmail.com>
2016-11-11 15:23:01 +0530

    Bug fix: icons loading in firefox slow - solution preload icons on init

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ and story media view - sub views for media is defined in js/views.js*/
 		this.footerView = new app.FooterView();
 		this.footerView.render();
 		Backbone.history.start();
+
+		//loading a test image on router init so that the .htaccess login dialog will open on site launch
+		//Loading the icons in background - as the load times in firefox was too slow - issue#36
+		_.each(["https://www.ncbs.res.in/ncbs25/omeka/files/thumbnails/72e51417b77adaafccc6798ab6b2b136.jpg", "img/Assets/identity_inside-01.png", "img/Assets/instituin_inside-01.png",
+					"img/Assets/growth_inside.png", "img/Assets/research-inside.png", "img/Assets/education_inside1.png",
+					 "img/Assets/ripple_inside.png", "img/Assets/intersections_inside.png"], function(imgSrc){
+					 	var backLoad = new Image();
+					 	backLoad.src = imgSrc;
+				});
 	};
 
 // require to import the html files where the templates are declared for the view partials
